3080b4ee8385d3ab8c9467407914cdc237faf4f3
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-06-30 01:39:12 -0400

    data type conversions

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score ="33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+let convertnum=Number(score);
+//console.log(typeof(convertnum));
+//console.log(convertnum);
+
+let status="deepak is logged in";
+let num1=Number(status);
+console.log(typeof num1);
+console.log(num1);
