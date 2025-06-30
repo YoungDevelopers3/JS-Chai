9b9ce4a1c4b2d2e64e6ad24c3f9bf73ab7872751
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-06-30 02:25:44 -0400

    some variables changed

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -5,7 +5,8 @@ let convertnum=Number(score);
 //console.log(typeof(convertnum));
 //console.log(convertnum);
 
-let status="33";
-let num1=Number(status);
-console.log(typeof num1);
-console.log(num1);
+//let status="33";
+//let num1=Number(status);
+//console.log(typeof num1);
+//console.log(num1);
+console.log(2+"1"+2);
