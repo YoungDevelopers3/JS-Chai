bbc0516d9595942b27563feeffcf700dc2fe72b7
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-06-30 01:08:09 -0400

    data types

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+//alert(3+3)// we are using node.js ,not browser so in vs code it will give error.
+console.log(3
+    +          //not a nice practice for code readability.
+3)
+
+console.log(typeof "Hitesh")
