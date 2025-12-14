13cfb6f33b2de50e4a415af7bd82b30a7abcf667
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-14 18:09:31 -0500

    strings

---
+++ 01_Basics/stringi.js
@@ -0,0 +1,13 @@
+let username="Silent_Shark";
+let total_commits=50;
+
+//console.log(`Hi my name is ${username} and my total github commits are ${total_commits}`);
+
+const gameName = new String('Football');
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('o'));
+
+//console.log(gameName.substring(1,4));
+console.log(gameName.slice(-4,-1));
