bc019a241d5bbee8a6fc9efba6129790b1df6430
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-14 01:42:33 -0500

    some changes

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=8899562;
+let accountEmail="deepak.bhatti@goodcompany.com";
+var accountPassword="something";
+accountCity="Chand pai hai apun";
+
+//accountId=2;
+accountEmail="deepak.bhatti@dreamCompany.com";
+accountPassword="topSecret";
+accountCity="San Fransisco";
+let accountState
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
