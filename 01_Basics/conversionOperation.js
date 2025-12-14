68d969a41183c80bb526930baa14f77a9fb7b5ac
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-14 02:39:49 -0500

    data types

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,18 @@
+let score ="33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof NaN);
+
+let loggedin=0;
+// let isUserLoggedIn=Boolean(loggedin);
+// console.log(typeof isUserLoggedIn);
+// console.log(isUserLoggedIn);
+
+let invest="money";
+let asset=Number(invest);
+console.log(typeof asset);
+console.log(asset);
