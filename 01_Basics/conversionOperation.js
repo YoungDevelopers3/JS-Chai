7a3127da775d4a8dd2e4b255d1c58287f82ad2b5
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-14 14:58:11 -0500

    data types

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -14,5 +14,31 @@ let loggedin=0;
 
 let invest="money";
 let asset=Number(invest);
-console.log(typeof asset);
-console.log(asset);
+// console.log(typeof asset);
+// console.log(asset);
+
+//**************Operations************************/
+
+// console.log(1+"2");
+// console.log("2"+1);
+// console.log(1+"2"+3);
+
+// let gamecounter=100;
+// gamecounter++;
+// console.log(gamecounter);
+
+// let gamecounter=100;
+// ++gamecounter;
+// console.log(gamecounter);
+
+// console.log("2" >1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null==undefined);
+
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<0);
