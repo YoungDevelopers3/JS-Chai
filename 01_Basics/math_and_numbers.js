21950b19fb71cd98c6e8deea78ba477a2050f2f9
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-15 00:10:34 -0500

    maths and numbers

---
+++ 01_Basics/math_and_numbers.js
@@ -0,0 +1,14 @@
+const score=400
+//console.log(score);
+
+const balance =new Number(100)
+//console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//two decimal places
+
+//console.log(Math.floor(Math.random()*10));
+
+const min=1;
+const max=6;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
