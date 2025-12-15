73d281eddc312f46725698e7fff5858e667e6d39
YoungDevelopers3 <deepakbhatti889@gmail.com>
2025-12-15 01:40:01 -0500

    dates

---
+++ 01_Basics/calendar.js
@@ -0,0 +1,9 @@
+let tareek= new Date();
+// console.log(tareek);
+// console.log(tareek.toString());
+// console.log(tareek.toDateString());
+// console.log(tareek.toLocaleString());
+// console.log(typeof tareek);
+let another_date=new Date(1999,2,28);
+//console.log(another_date.toLocaleDateString());
+console.log(Math.floor(Date.now()/1000));
