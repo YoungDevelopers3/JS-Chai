db5045588219ec34598861d53a7e7259d1405aef
YoungDevelopers3 <deepakbhatti889@gmail.com>
2026-03-27 20:52:14 -0400

    Data type conversion

---
+++ 01_Basics/vedio6.js
@@ -0,0 +1,10 @@
+let score="33abc"
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let value="                       "
+let strToBool=Boolean(value);
+console.log(strToBool);
